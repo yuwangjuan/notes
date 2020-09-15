a5acb9ccf6554f65ec33a96fcea91627242d8387
wjyu <wjyu@iflytek.com>
2020-09-15 16:23:23 +0800

    节流：throttle

---
+++ vuedemos/src/components/Throotte.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- // lodash,节流 -->
+  <div
+    id="container"
+    @scroll="scrollUp($event)"
+    style="width:100%;height:1400px;"
+  ></div>
+</template>
+<script>
+import _ from "lodash-es";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    window.addEventListener("scroll", _.throttle(this.handle, 2000), true);
+  },
+  methods: {
+    handle() {
+      console.log(Math.random());
+    }
+  }
+};
+</script>
+<style scoped></style>
