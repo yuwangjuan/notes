2b6c55244d25c878bfd14da9f70719b567f46d2d
wjyu <wjyu@iflytek.com>
2020-09-15 16:22:51 +0800

    防抖debounce

---
+++ vuedemos/src/components/Debounce.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+<!-- // lodash,防抖 -->
+    <div>
+       <input type="text" v-model="val" @input="getData($event)">     
+    </div>
+</template>
+<script>
+import _ from "lodash-es"
+export default {
+    data(){
+        return{
+            val:''
+        }
+    },
+    mounted(){
+        this.getData = _.debounce(this.getData, 5000)
+    },
+    methods:{
+        getData(e){
+            console.log(e,this.val,'e')
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
